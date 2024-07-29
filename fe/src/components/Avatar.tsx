4386bc193017bc87983b2426afd8ba8abcbe3b4f
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-07-30 00:38:15 +0530

    deploy9

--- fe/src/components/Avatar.tsx
+++ fe/src/components/Avatar.tsx
@@ -6,7 +6,7 @@ interface AvatarProps {
 function Avatar({ author, size }: AvatarProps) {
   return (
     <div className={`relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 ${size === "small" ? "w-6 h-6" : size === "medium" ? "w-8 h-8" : "w-10 h-10"}`}>
-      <span className="font-medium text-gray-600 dark:text-gray-300">{author[0].toUpperCase()}</span>
+      <span className="font-medium text-gray-600 dark:text-gray-300">{author[0]}</span>
     </div>
   )
 }
